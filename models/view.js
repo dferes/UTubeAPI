68e4d84b5d6921bb7ac4db19aa1d019e0b370f71
LOGIK-64 <dferes23@gmail.com>
2021-05-28 16:50:08 -0700

    All class methods for interacting with the View table of the db

---
+++ models/view.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const db = require('../db');
+const { NotFoundError, BadRequestError } = require('../expressError');
+
+class View {
+
+  /** Creates a new View with the data: 
+   *  { username, videoId }
+   * 
+   *  Returns: 
+   *  { id, createdAt, username, videoId }
+   */
+   static async create(viewData) {
+    const { username, videoId } = viewData;
+
+    const result = await db.query(
+      `INSERT INTO views ( 
+        username, 
+        video_id)
+      VALUES ($1,$2)
+      RETURNING 
+        id, created_at AS "createdAt", username, video_id AS "videoId"`,
+      [username, videoId]  
+    );
+    const view = result.rows[0];
+
+    return view;
+  }
+
+
+/** Retrieves a list of the 500 most rececnt video views added to the 
+ *  views table if no filter is provided, otherwise, a list of 
+ *  videos relative to one of the filters, limited to 500 records.
+ * 
+ *  Filter can be either one of: { username, videoId }
+ * 
+ * Returns:
+ *   { id, createdAt, username, videoId },...
+ * ]
+ */
+  static async getAll(filter={}) {
+    if ( Object.keys(filter).length && !(filter.username || filter.videoId) ){
+      throw new BadRequestError("Filter must be either 'username' or 'videoId'");  
+    }
+    
+    let conditinalSQLInsert = { 
+        username: ` WHERE username='${filter? filter.username : null}'`, 
+        videoId: ` WHERE video_id='${filter? filter.videoId : null}'`
+    };
+
+    let searchQuery =
+      `SELECT
+        id,
+        created_at AS "createdAt",
+        username,
+        video_id AS "videoId",
+      FROM views`;  
+
+    if(filter.username) searchQuery += conditinalSQLInsert.username;
+    else if (filter.videoId) searchQuery += conditinalSQLInsert.videoId;
+    searchQuery += '\nORDER BY created_at\nLIMIT 500';
+      
+    const result = await db.query(searchQuery);
+    return result.rows;
+  }
+
+}
+
+module.exports = View;
