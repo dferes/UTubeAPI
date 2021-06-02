05b9fb8d21a6e7a6f80eb78c73ee1d872996edcc
Dylan Feres <dferes23@gmail.com>
2021-06-02 15:41:23 -0700

    endpoint for authenticating a user and returning a JWT with the users signed data

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,31 @@ const User = require('../models/user');
 const { BadRequestError } = require('../expressError');
 
 const jsonschema = require('jsonschema');
+const userAuthSchema = require('../schemas/userAuthSchema.json')
 const { makeToken } = require('../utility_functions/token');
+
+/** POST /auth/token:  { username, password } => { token }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
+ */
+router.post('/token', async (req, res, next) => {
+  try {
+    const validator = jsonschema.validate(req.body, userAuthSchema);
+    if (!validator.valid) throw new BadRequestError(validator.errors.map(e => e.stack));
+
+    const { username, password } = req.body;
+    const user = await User.authenticate(username, password);
+    const token = makeToken(user);
+
+    return res.json({ token });
+  } catch(err) {
+    return next(err);  
+  }
+
+});
+
 
 
 module.exports = router;
