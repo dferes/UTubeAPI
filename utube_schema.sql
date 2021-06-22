1a02ccd31650d527489afcdc37d420a32ed5be12
Dylan Feres <dferes23@gmail.com>
2021-06-21 22:36:11 -0700

    Ready to redeploy

--- utube_schema.sql
+++ utube_schema.sql
@@ -71,47 +71,47 @@ CREATE TABLE Views (
 );
 
 
-INSERT INTO Users (username, password, first_name, last_name, email)
-VALUES ('testUser1', 'password', 'Mr.', 'Test', 'testemai1@gmail.com'),
-       ('testUser2', 'password', 'Some.', 'Guy', 'someGuy@gmail.com'),
-       ('testUser3', 'password', 'Robot', 'Person', 'mrRobot@gmail.com');
+-- INSERT INTO Users (username, password, first_name, last_name, email)
+-- VALUES ('testUser1', 'password', 'Mr.', 'Test', 'testemai1@gmail.com'),
+--        ('testUser2', 'password', 'Some.', 'Guy', 'someGuy@gmail.com'),
+--        ('testUser3', 'password', 'Robot', 'Person', 'mrRobot@gmail.com');
 
-INSERT INTO Videos (title, description, url, username)
-VALUES ('Sintel', 'Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4', 'testUser1'),
-       ('Tears of Steel', 'Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4', 'testUser2'),
-       ('Big Buck Bunny', 'Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit aint no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', 'testUser3'),
-       ('Elephant Dream', 'he first Blender Open Movie from 2006', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4', 'testUser1'),
-       ('For Bigger Blazes', 'HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4', 'testUser2'),
-       ('For Bigger Fun', 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For $35.  Find out more at google.com/chromecast.', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4', 'testUser3');
+-- INSERT INTO Videos (title, description, url, username)
+-- VALUES ('Sintel', 'Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4', 'testUser1'),
+--        ('Tears of Steel', 'Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4', 'testUser2'),
+--        ('Big Buck Bunny', 'Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit aint no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', 'testUser3'),
+--        ('Elephant Dream', 'he first Blender Open Movie from 2006', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4', 'testUser1'),
+--        ('For Bigger Blazes', 'HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4', 'testUser2'),
+--        ('For Bigger Fun', 'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For $35.  Find out more at google.com/chromecast.', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4', 'testUser3');
 
-INSERT INTO VideoLikes (username, video_id)
-VALUES ('testUser1', 1),
-       ('testUser2', 2),
-       ('testUser2', 3),
-       ('testUser3', 3);
+-- INSERT INTO VideoLikes (username, video_id)
+-- VALUES ('testUser1', 1),
+--        ('testUser2', 2),
+--        ('testUser2', 3),
+--        ('testUser3', 3);
 
-INSERT INTO Subscriptions (subscriber_username, subscribed_to_username)
-VALUES ('testUser1', 'testUser2'),
-       ('testUser1', 'testUser3'),
-       ('testUser2', 'testUser1'),
-       ('testUser2', 'testUser3'),
-       ('testUser3', 'testUser1'),
-       ('testUser3', 'testUser2');
+-- INSERT INTO Subscriptions (subscriber_username, subscribed_to_username)
+-- VALUES ('testUser1', 'testUser2'),
+--        ('testUser1', 'testUser3'),
+--        ('testUser2', 'testUser1'),
+--        ('testUser2', 'testUser3'),
+--        ('testUser3', 'testUser1'),
+--        ('testUser3', 'testUser2');
 
-INSERT INTO Comments (video_id, username, content)
-VALUES (1, 'testUser1', 'Nice video man'),
-       (1, 'testUser2', 'You suck'),
-       (1, 'testUser3', 'hahahahaha'),
-       (2, 'testUser1', 'meh, not funny'),
-       (2, 'testUser2', 'simpsons did it'),
-       (3, 'testUser3', 'Look, a comment');
+-- INSERT INTO Comments (video_id, username, content)
+-- VALUES (1, 'testUser1', 'Nice video man'),
+--        (1, 'testUser2', 'You suck'),
+--        (1, 'testUser3', 'hahahahaha'),
+--        (2, 'testUser1', 'meh, not funny'),
+--        (2, 'testUser2', 'simpsons did it'),
+--        (3, 'testUser3', 'Look, a comment');
 
-INSERT INTO Views (video_id, username)
-VALUES (1, 'testUser1'),
-       (1, 'testUser2'),
-       (1, 'testUser3'),
-       (2, 'testUser1'),
-       (2, 'testUser3'),
-       (2, 'testUser3'),
-       (3, 'testUser1'),
-       (3, 'testUser3');
+-- INSERT INTO Views (video_id, username)
+-- VALUES (1, 'testUser1'),
+--        (1, 'testUser2'),
+--        (1, 'testUser3'),
+--        (2, 'testUser1'),
+--        (2, 'testUser3'),
+--        (2, 'testUser3'),
+--        (3, 'testUser1'),
+--        (3, 'testUser3');
